4676d4f0c7a6bf4215bba623276be6aba8fadd35
Exsper <exsper@163.com>
2020-03-11 13:05:48 +0800

    fix command and events filepath

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const thisPath = __dirname;
 module.exports.name = 'osuercalendar';
 // 插件处理和输出
 module.exports.apply = (ctx, options = {}) => {
-    const filePath = options.filePath || path.join(thisPath, "events.json");
+    const filePath = options.filePath || path.join(thisPath, "../../events.json");
     const users = options.users;
     let eventPath = "";
     let sameplePath = path.join(thisPath, "./eventsSample.json");
@@ -29,12 +29,14 @@ module.exports.apply = (ctx, options = {}) => {
         });
     ctx.command('增加活动 <arg1> <arg2> <arg3>')
         .action(({ meta }, arg1, arg2, arg3) => {
-            if (!!users && users.indexOf(meta.userId) < 0) return meta.$send("抱歉，您没有权限修改活动");
+            if (!(arg1 && arg2 && arg3)) return meta.$send("请输入正确参数：增加活动 活动名称 宜详情 忌详情");
+            else if (!!users && users.indexOf(meta.userId) < 0) return meta.$send("抱歉，您没有权限修改活动");
             else return eventsJson.addEvent(meta, eventPath, arg1, arg2, arg3);
         });
     ctx.command('删除活动 <arg1>')
         .action(({ meta }, arg1) => {
-            if (!!users && users.indexOf(meta.userId) < 0) return meta.$send("抱歉，您没有权限修改活动");
+            if (!arg1) return meta.$send("请输入正确参数：删除活动 活动名称");
+            else if (!!users && users.indexOf(meta.userId) < 0) return meta.$send("抱歉，您没有权限修改活动");
             else return eventsJson.delEvent(meta, eventPath, arg1);
         });
 };
